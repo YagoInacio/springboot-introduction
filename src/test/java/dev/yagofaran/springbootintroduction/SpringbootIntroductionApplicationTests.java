0902ffb1e65c59440b5a57912a7feaf80915ac8d
Yago Inacio <yagofaran@gmail.com>
2023-11-17 17:56:57 -0300

    first commit

---
+++ src/test/java/dev/yagofaran/springbootintroduction/SpringbootIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yagofaran.springbootintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
